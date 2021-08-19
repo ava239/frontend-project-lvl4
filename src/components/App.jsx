2bbbe38827943045a3fa8244fdb409239f533e34
Ava <ava239@yandex.ru>
2021-08-20 02:49:19 +0300

    add auth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,87 @@
 // @ts-check
 
-import React from 'react';
-import Menu from './Menu.jsx';
+import React, { useContext, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
+import {
+  Button,
+  Navbar,
+  Nav,
+  Container,
+} from 'react-bootstrap';
+import LoginPage from './LoginPage.jsx';
+import MainPage from './MainPage.jsx';
+import authContext from '../contexts/index.jsx';
+import useAuth from '../hooks/index.jsx';
+import NoMatch from './NoMatchPage.jsx';
 
-const App = () => (
-  <>
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+
+  return (
+    <authContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </authContext.Provider>
+  );
+};
+
+const PrivateRoute = ({ children, path }) => {
+  const auth = useAuth();
+
+  return (
+    <Route
+      path={path}
+      render={({ location }) => (auth.loggedIn
+        ? children
+        : <Redirect to={{ pathname: '/login', state: { from: location } }} />)}
+    />
+  );
+};
+
+const AuthButton = () => {
+  const auth = useContext(authContext);
+
+  return (
+    auth.loggedIn
+      ? <Button onClick={auth.logOut}>Log out</Button>
+      : null
+  );
+};
+
+export default () => (
+  <AuthProvider>
     <div className="d-flex flex-column h-100">
-      <Menu />
+      <Router>
+        <Navbar bg="light" expand="lg" className="shadow-sm">
+          <Container>
+            <Navbar.Brand as={Link} to="/">Chat</Navbar.Brand>
+            <Nav className="mr-auto" />
+            <AuthButton />
+          </Container>
+        </Navbar>
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <PrivateRoute exact path="/">
+            <MainPage />
+          </PrivateRoute>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  </>
+  </AuthProvider>
 );
-export default App;
