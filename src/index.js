2fb83a417e91786b840e533ab2f1b0f4161b5f3d
Ava <ava239@yandex.ru>
2021-08-30 02:13:25 +0300

    change rollbar

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
 
 import '../assets/application.scss';
+import Rollbar from 'rollbar';
 import init from './init.jsx';
 import './i18n/index.js';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
+
+Rollbar.init({
+  accessToken: '71863b44bef048699273abc48716ce56',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: { environment: window.location.host },
+});
 
 init();
